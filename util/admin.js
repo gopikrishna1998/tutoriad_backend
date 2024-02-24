b0561300e9c6cb1a84ccf357662fb17283573e6c
unknown <gopikrishna1998@gmail.com>
2024-02-24 14:14:00 +0530

    Merging the changes

--- util/admin.js
+++ util/admin.js
@@ -1,9 +1,10 @@
 var admin = require("firebase-admin")
 
-var serviceAccount = require("")
+var serviceAccount = require("C:/Users/Hp/OneDrive/Documents/tutoriad_backend/util/tutor-iad-firebase-adminsdk-bsjrw-1e997ae4a5.json")
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://tutor-iad-default-rtdb.firebaseio.com"
 });
 
 const db = admin.firestore();
