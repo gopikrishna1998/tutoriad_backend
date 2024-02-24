d7c4b9ab14f99fef39d1e85ec64791f2a2fe5288
unknown <gopikrishna1998@gmail.com>
2024-02-24 14:03:05 +0530

    Trying to integrate with firebase

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ res.send('This is my demo project')
 })
 app.listen(PORT, function () {
 console.log(`Demo project at: ${PORT}!`); });
+
+const { books } = require('./handlers/books')
+app.get('/books', books);
